b48ba1f10e6d7a9b0427e603f8975b80be8dd57b
rkdghwnd <ghwnd6448@gmail.com>
2024-05-20 20:42:53 +0900

    fix: 이미지 업로드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,104 @@
-import { useRef, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useRef, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  
-  const uploadInputRef = useRef(null)
-  const buttonRef = useRef(null)
+  const uploadInputRef = useRef(null);
 
-  const [imageURL, setImageURL] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [imageURL, setImageURL] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [inputFiles, setInputFiles] = useState([]);
+  const [token, setToken] = useState('');
+  const [des, setDes] = useState('black top shirt and white short pants');
+
+  const onChangeFileInput = async (e) => {
+    setInputFiles([e.currentTarget.files[0], e.currentTarget.files[1]]);
+  };
+
+  const createMixedImage = async () => {
+    setLoading(true);
+
+    const formData1 = new FormData();
+
+    formData1.append('file', inputFiles[0]);
+
+    const imageOneRes = await axios.post(
+      `https://api.tinytingel.ai/api/v2/user/upload-image`,
+      formData1,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const formData2 = new FormData();
+
+    formData2.append('file', inputFiles[1]);
+
+    const imageTwoRes = await axios.post(
+      `https://api.tinytingel.ai/api/v2/user/upload-image`,
+      formData2,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const creatImageresponse = await axios.post(
+      `https://api.tinytingel.ai/api/v2/user/cloth/create`,
+      {
+        garm_img: imageOneRes.data.data, // 바꿀 옷의 이미지
+        human_img: imageTwoRes.data.data, // 원본 사람의 이미지
+        garment_des: des, // 프롬프트
+      },
+    );
+    setImageURL(creatImageresponse.data.data);
+
+    setLoading(false);
+  };
+
+  const onChangeDes = (e) => {
+    setDes(e.currentTarget.value);
+  };
+
+  const onChangeToken = (e) => {
+    setToken(e.currentTarget.value);
+  };
 
   return (
     <>
-       {/* <button ref={buttonRef} onClick={() => {
+      {/* <button ref={buttonRef} onClick={() => {
         uploadInputRef.current.click()
        }}>이미지 업로드</button> */}
-       
-       
-       {loading ? '로딩중...': <><input type="file" multiple ref={uploadInputRef} onChange={async (e) => {
-        console.log(e.currentTarget.files)
 
-        setLoading(true)
-        const response = await axios.post(`https://api.tinytingel.ai/api/v2/user/cloth/create`,{
-          "garm_img": "https://replicate.delivery/pbxt/KgwTlZyFx5aUU3gc5gMiKuD5nNPTgliMlLUWx160G4z99YjO/sweater.webp", // 바꿀 옷의 이미지
-          "human_img": "https://replicate.delivery/pbxt/KgwTlhCMvDagRrcVzZJbuozNJ8esPqiNAIJS3eMgHrYuHmW4/KakaoTalk_Photo_2024-04-04-21-44-45.png", // 원본 사람의 이미지
-          "garment_des": "blue top white short pants" // 프롬프트
-      })
+      {loading ? (
+        '로딩중...'
+      ) : (
+        <>
+          <input
+            type="file"
+            multiple
+            ref={uploadInputRef}
+            onChange={onChangeFileInput}
+          />
+          <div>
+            <label>설명</label>
+            <input type="text" onChange={onChangeDes} />
+          </div>
 
-      console.log(response.data.data)
-      setImageURL(response.data.data)
-      setLoading(false)
-       }}/><img src={imageURL}/></>}
+          <div>
+            <label>토큰</label>
+            <input type="text" onChange={onChangeToken} />
+          </div>
+
+          <button onClick={createMixedImage}>생성하기</button>
+          <img src={imageURL} />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
